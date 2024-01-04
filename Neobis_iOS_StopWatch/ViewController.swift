bbf58e36d57df54ca85170aad0a4dfdbb7e60384
Igor Pachkin <altair1901@icloud.com>
2024-01-04 18:24:20 +0100

    created a project

---
+++ Neobis_iOS_StopWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_StopWatch
+//
+//  Created by Игорь Пачкин on 30/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
