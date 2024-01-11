327621f6ca5cb48ca3a8b6aa93e7d3185f4f92cc
Igor Pachkin <altair1901@icloud.com>
2024-01-11 13:18:34 +0100

    feat: add UIImageView, add logic for switch image, started writing timer logic

--- Neobis_iOS_StopWatch/StopWathcController.swift
+++ Neobis_iOS_StopWatch/StopWathcController.swift
@@ -11,7 +11,8 @@ class StopWathcController: UIViewController, UIPickerViewDelegate, UIPickerViewD
 
     
     // MARK: - interface elements
-    @IBOutlet weak var stopWatch: UISegmentedControl!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var logoImage: UIImageView!
     
     @IBOutlet weak var timerLabel: UILabel!
     
@@ -26,6 +27,9 @@ class StopWathcController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     
     var time: Timer?
     var count = 0
+    
+    var countDownSeconds: Int = 0
+    
     var isTimerRunning = false
     
     
@@ -39,16 +43,47 @@ class StopWathcController: UIViewController, UIPickerViewDelegate, UIPickerViewD
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //updateTimerLabel()
         
-        
-        
-        updateTimerLabel()
+        segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
         
         pickerView.delegate = self
         pickerView.dataSource = self
         
+        updateUIForSelectedSegment()
+        
     }
     
+    @objc func segmentedControlValueChanged() {
+        // Вызывается при изменении значения UISegmentedControl
+        updateUIForSelectedSegment()
+    }
+
+    func updateUIForSelectedSegment() {
+        // Обновляем UI в зависимости от выбранного сегмента
+        let selectedIndex = segmentedControl.selectedSegmentIndex
+        if selectedIndex == 0 {
+            // Режим секундомера
+            timerLabel.text = "00:00:00"
+            pickerView.isHidden = true
+            startButton.isEnabled = true
+            stopButton.isEnabled = false
+            resetButton.isEnabled = false
+            logoImage.image = UIImage(systemName: "stopwatch")
+            //updateTimerLabel() //???
+        } else {
+            // Режим пикера
+            pickerView.isHidden = false
+            startButton.isEnabled = false
+            stopButton.isEnabled = true
+            resetButton.isEnabled = true
+            logoImage.image = UIImage(systemName: "timer")
+            updateTimerLabelFromPicker()
+        }
+
+        // Сброс секундомера, если он был запущен
+        stopTimer()
+    }
     func updateTimerLabel() { // обновление времени на лэйбле
 
         time = Timer.scheduledTimer(timeInterval: 0.01,
@@ -60,12 +95,18 @@ class StopWathcController: UIViewController, UIPickerViewDelegate, UIPickerViewD
        }
 
     @IBAction func startButtonTapped(_ sender: UIButton) {
-        if isTimerRunning {
-            // Секундомер уже работает, значит это действие будет паузой
-            stopTimer()
+        
+        if segmentedControl.selectedSegmentIndex == 0 {
+            if isTimerRunning {
+                // Секундомер уже работает, значит это действие будет паузой
+                stopTimer()
+            } else {
+                // Секундомер не работает, начинаем его
+                startTimer()
+            }
         } else {
-            // Секундомер не работает, начинаем его
-            startTimer()
+            // Запуск отсчета времени из пикера
+            startCountdownFromPicker()
         }
 
     }
@@ -122,6 +163,25 @@ class StopWathcController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     func makeTimeString(minutes: Int, seconds: Int, milliseconds: Int) -> String {
         let timeString = String(format: "%02d:%02d:%02d", minutes, seconds, milliseconds % 1000 / 10)
         return timeString
+    }
+    
+    //MARK: - Picker
+    func updateTimerLabelFromPicker(){
+        let selectedRowHour = pickerView.selectedRow(inComponent: 0)
+        let selectedRowMinute = pickerView.selectedRow(inComponent: 1)
+        let selectedRowSecond = pickerView.selectedRow(inComponent: 2)
+        
+        countDownSeconds = selectedRowHour * 3600 + selectedRowMinute * 60 + selectedRowSecond
+        timerLabel.text = String(format: "%02d:%02d:%02d", selectedRowHour, selectedRowMinute, selectedRowSecond)
+    }
+    
+    func startCountdownFromPicker() {
+        // Запуск отсчета времени из пикера
+        time = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        isTimerRunning = true
+        startButton.isEnabled = false
+        stopButton.isEnabled = true
+        resetButton.isEnabled = true
     }
     
     // MARK: - UIPickerViewDataSource
